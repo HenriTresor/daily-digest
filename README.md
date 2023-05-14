7b837f4298885c03eef04ac32d62d61fd049af7b
henritresor <shimwamanahenritresor@gmail.com>
2023-05-14 11:12:49 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+### Daily Digest
+
+### made with NEXT.JS and NEWS API
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import '@fontsource/roboto/400.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>homepage</h1>
+  </div>
+  )
+}
